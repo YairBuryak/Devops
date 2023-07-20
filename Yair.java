183230bbde6cf35d93b278ee78d8e32a44c3c196
Yair Buryak <iyairburyak2@gmail.com>
2023-07-20 13:07:26 +0300

    first commit

--- Yair.java
+++ Yair.java
@@ -1,7 +1,7 @@
 public class Yair{
 public static void main(String[] args) {
 for (int i=1;i<=10; i++) {
-System.out.println("Yaroslav Buryak Yair ..."+i+1);
+System.out.println("Yaroslav Buryak Yair ..."+i+10);
 }
 }
 }
